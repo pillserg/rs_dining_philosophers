7169ec96bbb926e30c1c832f8e72e5fe2895bfe4
Sergey Pilyavskiy <pill.sv0@gmail.com>
2015-06-01 11:57:03 +0300

    dumb eating

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string()
+        }
+    }
+
+    fn eat(&self) {
+        println!("{:?} is done eating", self.name );
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let philosophers = vec![
+        Philosopher::new("Judith Butler"),
+        Philosopher::new("Cilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Emma Goldman"),
+        Philosopher::new("Michel Foucault"),
+    ];
+    
+    for p in &philosophers {
+        p.eat();
+    }
+    
 }
